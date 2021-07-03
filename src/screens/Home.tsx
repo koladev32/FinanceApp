d24f8c22c838c3f938d982f11b9ac2858586496d
Kolawole <onaelmangabo@gmail.com>
2021-07-04 00:10:02 +0200

    - wip: add home screens and navigator

---
+++ src/screens/Home.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import {View, Text} from "react-native";
+import Layout from "../components/Layout";
+
+
+export default function HomeScreen () {
+    return (
+        <Layout>
+            <View>
+                <Text>
+                    Hello World.
+                </Text>
+            </View>
+        </Layout>
+    )
+}
